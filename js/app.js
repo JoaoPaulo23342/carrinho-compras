9b858d27c90389af4a3e0c2a2b030c0d0430c09a
João Paulo Resende <joaopauloresendecosta55@gmail.com>
2023-11-19 15:13:50 -0300

    Carrinho de compras

---
+++ js/app.js
@@ -0,0 +1,40 @@
+let totalGeral = 0;
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').textContent = 'R$ 0';
+function adicionar() {
+    //recuperar valores nome do produto, quantidade e valor 
+    let produto = document.getElementById('produto').value; 
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    let preco = quantidade * Number(valorUnitario);
+    let Lista_do_carrinho = document.getElementById('lista-produtos');
+    Lista_do_carrinho.innerHTML = Lista_do_carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+<span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+</section>`;
+
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total'); 
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.get.ElementById('quantidade').value = 0;
+
+}
+    
+
+//Código omitido
+
+
+
+function limpar() {
+    totalGeral = 0; 
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
+
+}
+
+
+
+
+    
+     
+
